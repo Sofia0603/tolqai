4f7e8b4c1d2e61c6414471780341ac3079dac85a
Sofia Mac <shestakova.sofia2016@mail.ru>
2025-11-04 23:58:45 +0400

    fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,17 +3,17 @@ import Footer from "@/components/Footer"
 import "./globals.css"
 
 export const metadata = {
-  title: "Мой лендинг",
-  description: "Одностраничный сайт с SSR на Next.js",
+  title: "tolqAi",
+  description: "",
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="ru">
     <body className="flex flex-col min-h-screen">
-    <Header />
-    <main className="flex-grow">{children}</main>
-    <Footer />
+      <Header />
+      <main className="flex-grow">{children}</main>
+      <Footer />
     </body>
     </html>
   )
--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,6 @@ import Hero from "@/components/sections/Hero"
 export default function Page() {
   return (
     <>
-
       <Hero />
     </>
   )
