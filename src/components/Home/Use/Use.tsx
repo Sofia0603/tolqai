a2cb8419eb90c6a510bb2c85108eacc72c451a0f
SofiaPCHome <shestakova.sofia2016@mail.ru>
2025-11-25 21:37:58 +0400

    added api

--- src/components/Home/Use/Use.tsx
+++ src/components/Home/Use/Use.tsx
@@ -8,46 +8,11 @@ interface IUseCase {
 }
 
 
-const useCasesData: IUseCase[] = [
-  {
-    icon: "/images/icons/icon-sale.svg",
-    title: "Retail & Automotive Sales",
-    description:
-        "From sales and HR to customer support and training, tolqAI delivers conversation intelligence across your entire organization",
-  },
-  {
-    icon: "/images/icons/icon-support.svg",
-    title: "Customer Support Centers",
-    description:
-        "Monitor online call quality across all channels, identify training needs, and ensure compliance with service standards",
-  },
-  {
-    icon: "/images/icons/icon-add-human.svg",
-    title: "HR & Recruitment Teams",
-    description:
-        "Record and analyze interviews for objective assessment, standardized evaluation, and data-driven hiring decisions",
-  },
-  {
-    icon: "/images/icons/icon-idea.svg",
-    title: "Training & QA Teams",
-    description:
-        "Analyze real conversations to identify best practices, coaching opportunities, and standardize training programs",
-  },
-  {
-    icon: "/images/icons/icon-star.svg",
-    title: "Hospitality & Service",
-    description:
-        "Enhance guest interactions across touchpoints, standardize service quality, and gather actionable feedback insights",
-  },
-  {
-    icon: "/images/icons/icon-location.svg",
-    title: "Multi-Location Teams",
-    description:
-        "Unified analytics across all locations and channels to compare performance, share insights, and scale best practices",
-  },
-];
+interface UseProps {
+  data: IUseCase[];
+}
 
-export default function Use(){
+export default function Use({ data }: UseProps) {
 
   return (
     <section className="bg-color-black pt-10 pb-12 md:pt-20 md:pb-21">
@@ -58,7 +23,7 @@ export default function Use(){
 
         <div className="flex flex-col gap-3 mb-20 md:grid md:grid-cols-2 md:mb-24 xl:grid-cols-3 xl:gap-5 xl:mb-35">
 
-          {useCasesData.map((item, index) => (
+          {data.map((item, index) => (
               <Card
                 key={index}
                 icon={item.icon}
